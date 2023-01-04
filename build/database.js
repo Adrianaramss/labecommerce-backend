ff0fc2e78399720f043141be2e23bd798ef3f8af
Adriana <adrianascosta9@gmail.com>
2023-01-04 18:26:59 -0300

    typescript-2

--- build/database.js
+++ build/database.js
@@ -1,6 +1,7 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.purchase = exports.products = exports.users = void 0;
+exports.purchase = exports.getAllPuchasesFromUserId = exports.createPuschase = exports.queryProductsByName = exports.getProductById = exports.getAllProducts = exports.createProduct = exports.products = exports.getAllUsers = exports.createUser = exports.users = void 0;
+const type_1 = require("./type");
 exports.users = [{
         id: '1',
         email: 'user1@gmail',
@@ -10,18 +11,84 @@ exports.users = [{
         email: 'user2@gmail',
         password: '100'
     }];
+function createUser(id, email, password) {
+    console.log(exports.users);
+    const newUser = {
+        id: id,
+        email: email,
+        password: password
+    };
+    exports.users.push(newUser);
+    console.log(exports.users);
+    console.log("Cadastro realizado com sucesso");
+}
+exports.createUser = createUser;
+function getAllUsers() {
+    return exports.users;
+}
+exports.getAllUsers = getAllUsers;
 exports.products = [{
         id: '1',
         name: 'Notebook',
         price: 3000,
-        category: 'technology'
+        category: type_1.Categorias.ELECTRONICS
     },
     {
         id: '2',
         name: 'Headphone Game',
         price: 472,
-        category: 'accessories gammer'
+        category: type_1.Categorias.ACCESSORIES
     }];
+function createProduct(id, name, price, category) {
+    console.log(exports.products);
+    const newProduct = {
+        id: id,
+        name: name,
+        price: price,
+        category: category
+    };
+    exports.products.push(newProduct);
+    console.log(exports.products);
+    console.log("produto criado com sucesso");
+}
+exports.createProduct = createProduct;
+function getAllProducts() {
+    return exports.products;
+}
+exports.getAllProducts = getAllProducts;
+function getProductById(idToSearch) {
+    return (exports.products.filter((product) => {
+        if (product.id === idToSearch) {
+            return product;
+        }
+    }));
+}
+exports.getProductById = getProductById;
+const queryProductsByName = (q) => {
+    const query = exports.products.filter((products) => {
+        return (products.name.toLocaleLowerCase().includes(q.toLocaleLowerCase()));
+    });
+    console.log(query);
+};
+exports.queryProductsByName = queryProductsByName;
+function createPuschase(userId, productId, quantity, totalPrice) {
+    const newPurchase = {
+        userId,
+        productId,
+        quantity,
+        totalPrice,
+    };
+    exports.purchase.push(newPurchase);
+    console.log(exports.purchase);
+    console.log("compra realizada com sucesso");
+}
+exports.createPuschase = createPuschase;
+const getAllPuchasesFromUserId = (userIdToSearch) => {
+    return exports.purchase.filter((purchase) => {
+        return (purchase.userId.toLocaleLowerCase().includes(userIdToSearch.toLocaleLowerCase()));
+    });
+};
+exports.getAllPuchasesFromUserId = getAllPuchasesFromUserId;
 exports.purchase = [{
         userId: "1",
         productId: "1",
--- src/database.ts
+++ src/database.ts
@@ -1,4 +1,6 @@
-import {TProduct, TUser, TPurchase}  from "./type";
+
+import { privateDecrypt } from "crypto";
+import {TProduct, TUser, TPurchase,Categorias}  from "./type";
 
 export const users: TUser[] = [{
     id: '1',
@@ -10,18 +12,103 @@ export const users: TUser[] = [{
     password: '100'
 }]
 
+
+export function createUser (id: string, email: string, password: string ) : void{
+    console.log(users)
+    const newUser: TUser = {
+    id: id,
+    email: email,
+    password: password
+    }
+users.push(newUser)
+console.log(users)
+console.log("Cadastro realizado com sucesso")
+
+}
+
+export function getAllUsers () : TUser [] {
+    return users
+}
+
+
+
 export const products: TProduct[] = [{
     id: '1',
     name: 'Notebook',
     price: 3000,
-    category: 'technology'
+    category: Categorias.ELECTRONICS
 },
 {
     id: '2',
     name: 'Headphone Game',
     price: 472,
-    category: 'accessories gammer'
+    category: Categorias.ACCESSORIES
+
 }]
+
+
+export function createProduct (id: string, name: string,  price:number, category: Categorias) : void{
+    console.log(products)
+    const newProduct: TProduct = {
+    id: id,
+    name: name,
+    price: price,
+    category:category
+    }
+    products.push(newProduct)
+    console.log(products)
+    console.log("produto criado com sucesso")
+
+}
+
+export function getAllProducts () : TProduct [] {
+    return products
+}
+
+
+export function getProductById (idToSearch: string) : TProduct[] | undefined{
+  return(products.filter((product) => {
+    if(product.id === idToSearch){
+    return product
+  }
+   } ) )
+  }
+
+export const queryProductsByName = (q:string) : void => {
+    const query = products.filter (
+        (products) => {
+            return (products.name.toLocaleLowerCase().includes(q.toLocaleLowerCase()))
+        }
+    )
+    console.log(query)
+}
+
+
+export function createPuschase (userId: string, productId: string,  quantity:number, totalPrice: number  ) : void{
+    const newPurchase: TPurchase = {
+    userId,
+    productId,
+    quantity,
+    totalPrice,
+
+    }
+    purchase.push(newPurchase)
+    console.log(purchase)
+    console.log("compra realizada com sucesso")
+
+}
+
+export const getAllPuchasesFromUserId = (userIdToSearch:string) :TPurchase[] => {
+    return purchase.filter (
+        (purchase) => { 
+            return(purchase.userId.toLocaleLowerCase(). includes(userIdToSearch.toLocaleLowerCase()))
+
+        }
+    )
+    }
+
+
+
 
 export const purchase: TPurchase[] = [{
     userId: "1",
@@ -38,3 +125,6 @@ totalPrice: products[1].price *2
 
 
 ]
+
+
+
